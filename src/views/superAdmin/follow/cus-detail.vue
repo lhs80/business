cb14830fe8cd45ea307f510093fa8144eefe19f0
LHS <1164738144@qq.com>
2019-12-15 14:11:04 +0800

    新增页面

--- src/views/superAdmin/follow/cus-detail.vue
+++ src/views/superAdmin/follow/cus-detail.vue
@@ -12,14 +12,38 @@
     </el-row>
     <el-tabs v-model="activeName" @tab-click="handleClick">
       <el-tab-pane label="基本资料" name="first">
-        
+        <ul>
+          <li>姓名：</li>
+          <li>电话：</li>
+          <li>地址：</li>
+          <li>品牌：</li>
+          <li>微信：</li>
+          <li>类型：</li>
+          <li>创建时间：</li>
+        </ul>
       </el-tab-pane>
-      <el-tab-pane label="客户订单" name="second">配置管理</el-tab-pane>
-      <el-tab-pane label="客户交易" name="third">角色管理</el-tab-pane>
-      <el-tab-pane label="跟进动态" name="fourth">定时任务补偿</el-tab-pane>
+      <el-tab-pane label="客户订单" name="second">
+        <el-table>
+          <el-table-column label="商品名称"></el-table-column>
+          <el-table-column label="订单金额"></el-table-column>
+          <el-table-column label="创建日期"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="客户交易" name="third">
+        <el-table>
+          <el-table-column label="累计交易"></el-table-column>
+          <el-table-column label="平均单价"></el-table-column>
+          <el-table-column label="累计订单"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="跟进动态" name="fourth">
+        <div>
+          <el-button size="small" type="primary">写跟进</el-button>
+        </div>
+        <div></div>
+      </el-tab-pane>
     </el-tabs>
   </section>
-
 </template>
 
 <script>
--- src/views/superAdmin/follow/list.vue
+++ src/views/superAdmin/follow/list.vue
@@ -1,37 +1,104 @@
 <template>
-  <div class="app-container calendar-list-container client-list">
-    <el-tabs v-model="activeName" @tab-click="handleClick">
+  <div style="position:relative" class="app-container calendar-list-container client-list">
+    <div class="btn-add-customer">
+      <el-button type="primary" size="small" @click="showExportCustomer=true">新增客户</el-button>
+    </div>
+    <el-tabs v-model="activeName">
       <el-tab-pane label="跟进客户" name="first">
         <!--菜单栏-->
         <div class="bg-white">
           <div>
             <span class="h5" style="display:inline-block;width:100px">客户类型：</span>
             <span>
-              <el-radio v-model="radio" label="1">备选项</el-radio>
-              <el-radio v-model="radio" label="2">备选项</el-radio>
+              <el-radio label="1">备选项</el-radio>
+              <el-radio label="2">备选项</el-radio>
             </span>
           </div>
           <div>
             <span class="h5" style="display:inline-block;width:100px">品 牌：</span>
             <span>
-              <el-radio v-model="radio" label="1">备选项</el-radio>
-              <el-radio v-model="radio" label="2">备选项</el-radio>
+              <el-radio label="1">备选项</el-radio>
+              <el-radio label="2">备选项</el-radio>
             </span>
           </div>
         </div>
         <hr>
         <el-row>
           <el-col :span="12">
-            <el-button type="primary" size="small" @click="showExportCustomer=true">新增客户</el-button>
             <el-button type="primary" size="small" @click="">编辑</el-button>
             <el-button type="primary" size="small" @click="changeGoodStatus(0)">转移至客户公海</el-button>
             <el-button type="danger" size="small" @click="passCheck()">删除</el-button>
-            <el-button type="primary" size="small" @click="passCheck()">添加订单</el-button>
             <el-button type="primary" size="small" @click="passCheck()">转移</el-button>
           </el-col>
           <el-col :span="12" class="text-right">
             <div style="width:150px;display:inline-block;">
-              <el-input size="small" v-model="txInfo.searchKey" placeholder="请输入关键字"></el-input>
+              <el-input size="small" v-model="txInfo.searchKey" placeholder="请输入客户姓名/手机号"></el-input>
+            </div>
+            <div style="display:inline-block;">
+              <el-button type="primary" size="small" @click="getCustomerList">搜索</el-button>
+            </div>
+          </el-col>
+        </el-row>
+        <el-table :data="cusList" stripe style="width: 100%">
+          <el-table-column type="selection"></el-table-column>
+          <el-table-column label="客户姓名">
+            <template slot-scope="scope">
+              <router-link to="/superAdmin/followList/detail">{{scope.row.name}}</router-link>
+            </template>
+          </el-table-column>
+          <el-table-column prop="tel" label="客户电话"></el-table-column>
+          <el-table-column prop="brand" label="品牌"></el-table-column>
+          <el-table-column prop="wxNo" label="客户微信"></el-table-column>
+          <el-table-column prop="type" label="客户地址"></el-table-column>
+          <el-table-column prop="orderNum" label="客户类型"></el-table-column>
+          <el-table-column prop="lastTime" label="距上次跟进"></el-table-column>
+          <el-table-column label="跟进记录">
+            <template slot-scope="scope">
+              <el-button type="primary" size="small" @click="showFollowRecord=true">写跟进</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <el-pagination
+          v-if="paginations.total > 0"
+          :page-sizes="paginations.pageSizes"
+          :page-size="paginations.page_size"
+          :layout="paginations.layout"
+          :total="paginations.total"
+          :current-page="paginations.page_index"
+          @current-change="handleCurrentChange"
+          @size-change="handleSizeChange"
+        ></el-pagination>
+      </el-tab-pane>
+      <el-tab-pane label="我的成交客户" name="second">
+        <!--菜单栏-->
+        <div class="bg-white">
+          <div>
+            <span class="h5" style="display:inline-block;width:100px">客户类型：</span>
+            <span>
+                      <el-radio label="1">备选项</el-radio>
+                      <el-radio label="2">备选项</el-radio>
+                    </span>
+          </div>
+          <div>
+            <span class="h5" style="display:inline-block;width:100px">品 牌：</span>
+            <span>
+                      <el-radio label="1">备选项</el-radio>
+                      <el-radio label="2">备选项</el-radio>
+                    </span>
+          </div>
+        </div>
+        <hr>
+        <el-row>
+          <el-col :span="12">
+            <el-button type="primary" size="small" @click="">编辑</el-button>
+            <el-button type="primary" size="small" @click="changeGoodStatus(0)">转移至客户公海</el-button>
+            <el-button type="danger" size="small" @click="passCheck()">删除</el-button>
+            <el-button type="primary" size="small" @click="showAddOrder=true">添加订单</el-button>
+            <el-button type="primary" size="small" @click="passCheck()">转移</el-button>
+          </el-col>
+          <el-col :span="12" class="text-right">
+            <div style="width:150px;display:inline-block;">
+              <el-input size="small" v-model="txInfo.searchKey" placeholder="请输入客户姓名/手机号"></el-input>
             </div>
             <div style="display:inline-block;">
               <el-button type="primary" size="small" @click="getCustomerList">搜索</el-button>
@@ -58,19 +125,6 @@
             </template>
           </el-table-column>
         </el-table>
-        <el-pagination
-          v-if="paginations.total > 0"
-          :page-sizes="paginations.pageSizes"
-          :page-size="paginations.page_size"
-          :layout="paginations.layout"
-          :total="paginations.total"
-          :current-page="paginations.page_index"
-          @current-change="handleCurrentChange"
-          @size-change="handleSizeChange"
-        ></el-pagination>
-      </el-tab-pane>
-      <el-tab-pane label="我的成交客户" name="second">
-        <!--菜单栏-->
         <el-pagination
           v-if="paginations.total > 0"
           :page-sizes="paginations.pageSizes"
@@ -115,7 +169,7 @@
     <el-dialog title="写跟进" :visible="showFollowRecord">
       <el-row>
         <el-col :span="6">
-          <el-select v-model="value" placeholder="请选择" size="small" style="width:200px">
+          <el-select placeholder="请选择" size="small" style="width:200px">
             <el-option label="微信" value="1"></el-option>
             <el-option label="电话" value="2"></el-option>
             <el-option label="其他" value="3"></el-option>
@@ -144,6 +198,83 @@
         <el-button type="primary" @click="saveCustomer()">保存</el-button>
       </div>
     </el-dialog>
+    <!--添加订单-->
+    <el-dialog title="添加订单" :visible="showAddOrder" top="5vh">
+      <el-row :gutter="10">
+        <el-col :span="12">
+          <div>
+            <label class="h6">选择品牌：</label>
+            <div style="display:inline-block">
+              <el-select v-model="brandName" placeholder="请选择" size="small">
+                <el-option label="微信" value="1"></el-option>
+                <el-option label="电话" value="2"></el-option>
+                <el-option label="其他" value="3"></el-option>
+              </el-select>
+            </div>
+          </div>
+          <div class="mt2">
+            <label class="h6">选择商品：</label>
+            <div style="display:inline-block">
+              <el-select v-model="product" placeholder="请选择" size="small">
+                <el-option label="微信" value="1"></el-option>
+                <el-option label="电话" value="2"></el-option>
+                <el-option label="其他" value="3"></el-option>
+              </el-select>
+            </div>
+          </div>
+          <div class="mt4">
+            <el-row v-for="(item,index) in norms" class="mt2">
+              <el-col :span="8">
+                <el-tag>{{item.name}}</el-tag>
+              </el-col>
+              <el-col :span="12">
+                <el-input-number v-model="item.orderNum" label="描述文字" size="small"></el-input-number>
+              </el-col>
+            </el-row>
+          </div>
+          <div class="text-center mt4">
+            <el-button type="primary" size="large" @click="addToCart">添加到进货单</el-button>
+          </div>
+        </el-col>
+        <el-col :span="12">
+          <h6>订单详情</h6>
+          <div class="cart-detail">
+            <el-table :data="cartList" size="small" stripe style="width: 100%">
+              <el-table-column prop="brand" label="品牌"></el-table-column>
+              <el-table-column prop="name" label="商品名称"></el-table-column>
+              <el-table-column prop="norms" label="规格数量">
+                <template slot-scope="scope">
+                  <el-row v-for="(item,index) in scope.row.norms" :key="index">
+                    <el-col :span="12">{{item.name}}</el-col>
+                    <el-col :span="12">{{item.orderNum}}</el-col>
+                  </el-row>
+                </template>
+              </el-table-column>
+            </el-table>
+          </div>
+          <div>
+            <label class="h6">商品总价:</label>
+            <div style="display:inline-block">
+              <el-input size="small" style="width:100px"></el-input>
+            </div>
+            <label class="h6 prl1">运费:</label>
+            <div style="display:inline-block">
+              <el-input size="small" style="width:100px"></el-input>
+            </div>
+          </div>
+          <div class="mt2">
+            <label class="h6">订单总价：</label>
+          </div>
+          <div class="mt2">
+            <label class="h6">收货地址：</label>
+          </div>
+        </el-col>
+      </el-row>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="showAddOrder = false">取消</el-button>
+        <el-button type="primary" @click="saveCustomer()">保存</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -152,11 +283,14 @@
   export default {
     data() {
       return {
+        product: '',//商品信息
+        brandName: '',//品牌信息
         followRecordTime: '',
         activeName: 'first',
         formLabelWidth: '120px',
         showExportCustomer: false,
         showFollowRecord: false,
+        showAddOrder: false,
         txList: [],
         timeRange: "",
         customerInfo: {
@@ -186,12 +320,41 @@
           pageSizes: [5, 10, 15, 20], //每页显示多少条
           layout: "total, sizes, prev, pager, next, jumper" // 翻页属性
         },
+        norms: [{
+          name: '黄色',
+          orderNum: 0
+        }, {
+          name: '绿色',
+          orderNum: 0
+        }, {
+          name: '红色',
+          orderNum: 0
+        }],
+        cartList: []
       }
     },
     mounted() {
 
     },
     methods: {
+      //添加到进货单
+      addToCart() {
+        this.cartList.push({
+          brand: '东西',
+          name: 'aaaa',
+          norms: [{
+            name: '黄色',
+            orderNum: 10
+          }, {
+            name: '黄色',
+            orderNum: 10
+          }, {
+            name: '黄色',
+            orderNum: 10
+          }]
+        });
+        console.log(this.cartList);
+      },
       getCustomerList() {
       },
       saveCustomer() {
@@ -210,3 +373,15 @@
     }
   };
 </script>
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .btn-add-customer {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+  }
+
+  .cart-detail {
+    height: 300px;
+    overflow: auto;
+  }
+</style>
